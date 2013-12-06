8180c5772fb727d4ce1d5b405d251fa90d97018d
Raphael Kolm <raphael.kolm@gmail.com>
2013-12-06 01:06:02 +0100

    Commentaires pour Javadoc

--- src/io/rkz/cacmsearch/CacmDatabase.java
+++ src/io/rkz/cacmsearch/CacmDatabase.java
@@ -7,8 +7,7 @@ import java.io.FileReader;
 import java.io.IOException;
 
 /**
- * CacmDatabase : représente une base de données CACM complète. Une CacmDatabase se construit à partir d'un fichier
- * CACM qui est parsé.
+ * Represents a complete CACM data file.
  */
 public class CacmDatabase
 {
@@ -16,12 +15,12 @@ public class CacmDatabase
     private ArrayList<Document> documents;
 
     /**
-     * Construct a CacmDatabase from a CACM text file.
+     * Construct a CacmDatabase by parsing a CACM data file.
      *
-     * @param cacmFileName Path to the CACM file.
-     * @param stopListFileName (optional) Path to a file containing stopwords, one per line.
+     * @param cacmFileName path to the CACM file
+     * @param stopListFileName path to a file containing stopwords (one word per line), or null to use no stop-list
      *
-     * @throws IOException if the CACM database could not be opened. No exception is thrown for the stop-list.
+     * @throws IOException if the CACM data file could not be opened. No exception is thrown for the stop-list.
      */
     public CacmDatabase(String cacmFileName, String stopListFileName) throws IOException
     {
@@ -39,23 +38,26 @@ public class CacmDatabase
     }
 
     /**
-     * Return the stop-list used for this database
-     * @return an ArrayList of the stop-list's words
+     * Return the stop-list words used when parsing the data file.
+     * @return an ArrayList with the stop-list's words
      */
     public ArrayList<String> getStopList() {
         return stopList;
     }
 
     /**
-     * Return the documents of the database
-     * @return an ArrayList of the documents
+     * Return the documents of the database.
+     * @return an ArrayList with the documents
      */
     public ArrayList<Document> getDocuments() {
         return documents;
     }
 
     /**
-     * Charge la stopList depuis un fichier dont le nom est passé en paramètre.
+     * Loads the stop-list words from a file. Each line should contain exactly one stop-word.
+     *
+     * @param stopListFile path to the stop-list file
+     * @throws IOException if the stop-list file could not be opened
      */
     private void loadStopList(String stopListFile) throws IOException
     {
@@ -67,7 +69,10 @@ public class CacmDatabase
     }
 
     /**
-     * Parse un fichier CACM et renvoie la liste de documents correspondante
+     * Loads a CACM data file into the database.
+     *
+     * @param cacmFileName path to the data file
+     * @throws IOException if the data file could not be opened
      */
     private void parseFile(String cacmFileName) throws IOException
     {
@@ -80,24 +85,24 @@ public class CacmDatabase
 
         while ((currentLine = br.readLine()) != null) {
 
-            // Début de document (.I <id>)
+            // Document start (.I <id>)
             if (currentLine.startsWith(".I")) {
                 if (currentDoc != null) documents.add(currentDoc);
                 int id = Integer.parseInt(currentLine.substring(3).trim());
                 currentDoc = new Document(id, null);
             }
 
-            // Début de champ T, W ou K : activer la lecture à partir de la prochaine ligne
+            // T, W ou K field: turn reading on, from next line
             else if (currentLine.startsWith(".T") || currentLine.startsWith(".W") || currentLine.startsWith(".K")) {
                 reading = true;
             }
 
-            // Autre champ : pas de lecture jusqu'au prochain champ
+            // Other field: do not read until the next field
             else if (currentLine.startsWith(".")) {
                 reading = false;
             }
 
-            // Pas de champ : lire si demandé
+            // Not a field mark: buffer content if reading is turned on
             else if (reading) {
                 currentDoc.addWords(tokenizeAndFilter(currentLine));
             }
@@ -107,16 +112,19 @@ public class CacmDatabase
     }
 
     /**
-     * Extrait les tokens ("mots") d'une ligne en supprimant la ponctuation et les majuscules. Supprime aussi les mots
-     * de la stop list.
+     * Extracts tokens (words) from a string, suppressing ponctuation and lowering case. Ignores words of the database's
+     * stop-list.
+     *
+     * @param input string to tokenize and filter
+     * @return an array of the string's tokens, lower-cased, without stop-words
      */
     private ArrayList<String> tokenizeAndFilter(String input)
     {
         input = input.trim().toLowerCase()
-                .replaceAll("[^a-z]", " ")  // remplacer tout ce qui n'est pas une lettre par un espace
-                .replaceAll("( )+", " ");   // réduire les espaces consécutifs à 1 espace
+                .replaceAll("[^a-z]", " ")  // replace all non-letters by a space
+                .replaceAll("( )+", " ");   // shrink consecutive spaces to 1 space
 
-        // Supprimer les mots de la stoplist
+        // Ignore stop-words
         String[] rawWords = input.split(" ");
         ArrayList<String> words = new ArrayList<String>();
         for (String word : rawWords) {
